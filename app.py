038f309f2668b763323809ba453a98a032a5f739
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-04-10 12:56:32 +0100

    added recipe endpoint

--- app.py
+++ app.py
@@ -8,6 +8,31 @@ def index():
     return "This is a recipe API, you can retrieve, and submit recipes"
 
 
+@app.route('/recipes')
+def recipes():
+    return {
+        "Recipes": {
+            "Beans On Toast": {
+                "Ingredients": ["Baked Beans", "Bread", "Butter"],
+                "Instructions": {
+                    1: "Cook the beans following package instructions",
+                    2: "Toast the Bread",
+                    3: "Butter the bread",
+                    4: "Put Beans on Bread",
+                    5: "Enjoy!"
+                }
+            },
+            "Cereal": {
+                "Ingredients": ["Cereal", "Milk"],
+                "Instructions": {
+                    1: "Get a bowl",
+                    2: "Pour cereal into bowl",
+                    3: "Pour Milk into bowl"
+                }
+            }
+        }
+    }
+
 
 
 if __name__ == '__main__':
