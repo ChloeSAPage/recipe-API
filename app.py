1e5172d9c7a03e2334850f96cdc96ee452b66514
Chloe Page <chloesapagepersonal@gmail.com>
2024-04-13 18:56:11 +0100

    added new endpoint

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def index():
     return "This is a recipe API, you can retrieve, and submit recipes"
 
-
-
+@app.route('/get-recipe')
+def get_recipe():
+    return "This is in development"
 
 if __name__ == '__main__':
     app.run(debug=True)
